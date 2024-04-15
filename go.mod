abf08402409cd5a14eba88811b5b1127525e83d2
Vitaliy Gadzhimagamedov <v.gadzhimagamedov@gmail.com>
2024-04-15 22:06:30 +0300

    add cli weeks before and after

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/vgadzh/go-calendar
+
+go 1.21.9
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli v1.22.14 // indirect
+)
--- main.go
+++ main.go
@@ -2,17 +2,52 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/urfave/cli"
 )
 
 func main() {
-	//now := time.Now()
-	now, err := time.Parse("2006-01-02", "2024-04-15")
-	if err != nil {
-		fmt.Println(err)
+	app := cli.NewApp()
+	app.Name = "Calendar"
+	app.Usage = "Prints current weekdays, optional previous weeks and optional next weeks"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "before, b",
+			Value: "0",
+			Usage: "Previous week count",
+		},
+		cli.StringFlag{
+			Name:  "after, a",
+			Value: "0",
+			Usage: "Next week count",
+		},
 	}
-	weekdayNumber := 1
-	for i := -7 + 1; i <= 28+7; i++ {
+	app.Action = func(c *cli.Context) error {
+		weeksBefore, err := strconv.Atoi(c.GlobalString("before"))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "weeksBefore parsing error %v", err)
+			return err
+		}
+		weeksAfter, err := strconv.Atoi(c.GlobalString("after"))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "weeksAfter parsing error %v", err)
+			return err
+		}
+
+		print_calendar(weeksBefore, weeksAfter)
+		return nil
+	}
+	app.Run(os.Args)
+
+}
+func print_calendar(weeksBefore, weeksAfter int) {
+	now := time.Now()
+	weekdayNumber := int(now.Weekday())
+
+	for i := 1 - 7*weeksBefore; i <= 7+7*weeksAfter; i++ {
 		newDay := now.AddDate(0, 0, i-weekdayNumber).Day()
 		//if newDay == now.Day() {
 		//	fmt.Printf("%s", "*")
@@ -22,4 +57,5 @@ func main() {
 			fmt.Println()
 		}
 	}
+
 }
