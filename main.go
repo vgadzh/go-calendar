127ba4841147d2a016b4048f451090be69a0beaf
Vitaliy Gadzhimagamedov <v.gadzhimagamedov@gmail.com>
2024-04-18 11:20:43 +0300

    add highlight for current day and month

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/fatih/color"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/urfave/cli"
@@ -28,6 +30,10 @@ func main() {
 			Name:  "print-month, m",
 			Usage: "Print current month",
 		},
+		cli.BoolFlag{
+			Name:  "print-colors, c",
+			Usage: "Print colored output",
+		},
 	}
 	app.Action = func(c *cli.Context) error {
 		weeksBefore, err := strconv.Atoi(c.GlobalString("before"))
@@ -41,8 +47,9 @@ func main() {
 			return err
 		}
 		printMonth := c.GlobalBool("print-month")
+		printColors := c.GlobalBool("print-colors")
 
-		printCalendar(weeksBefore, weeksAfter, printMonth)
+		printCalendar(weeksBefore, weeksAfter, printMonth, printColors)
 		//black := color.New(color.FgBlack)
 		//whiteBackground := black.Add(color.BgWhite).Add(color.Bold)
 		//whiteBackground.Println("Black text with white background.")
@@ -53,16 +60,32 @@ func main() {
 	app.Run(os.Args)
 
 }
-func printCalendar(weeksBefore, weeksAfter int, printMonth bool) {
+func printCalendar(weeksBefore, weeksAfter int, printMonth, printColors bool) {
 	now := time.Now()
-	weekdayNumber := int(now.Weekday())
 	if printMonth {
-		fmt.Println(now.Month())
+		if printColors {
+			color := color.New(color.Bold)
+			color.Println(now.Month())
+		} else {
+			fmt.Println(now.Month())
+		}
 	}
 
+	weekdayNumber := int(now.Weekday())
 	for i := 1 - 7*weeksBefore; i <= 7+7*weeksAfter; i++ {
-		newDay := now.AddDate(0, 0, i-weekdayNumber).Day()
-		fmt.Printf("%-5d", newDay)
+		newDate := now.AddDate(0, 0, i-weekdayNumber)
+		var str string
+		if printColors && newDate.Equal(now) {
+			black := color.New(color.FgBlack)
+			whiteBackground := black.Add(color.BgWhite).Add(color.Bold)
+			str = whiteBackground.Sprint(newDate.Day())
+		} else {
+			str = strconv.Itoa(newDate.Day())
+		}
+		tabSize := 5
+		tabs := tabSize - len(strconv.Itoa(newDate.Day()))
+		fmt.Print(str)
+		fmt.Print(strings.Repeat(" ", tabs))
 		if i%7 == 0 {
 			fmt.Println()
 		}
